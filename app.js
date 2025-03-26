9dbec66603092e858e30951897ec51d7d87d1c05
Rich <richbport@yahoo.com>
2025-03-26 12:43:42 -0400

    else if

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-let subscribed = true
+let subscribed = false
+let loggedIn = true
 
 if (subscribed === true) {
-    console.log('show video')
+    console.log('show the video')
+}
+else if (loggedIn === true) {
+    console.log('tell the user to upgrade their subscription')
+}
+else {
+    console.log('tell user to log into account')
 }
