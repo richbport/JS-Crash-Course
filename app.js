ff818145f3b970807c62c450e2a1081b1295f5f0
Rich <richbport@yahoo.com>
2025-03-27 12:51:20 -0400

    conditionals update

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-let cash = 35
+let cash = 50
 let price = 40
-let difference = cash - price
+let isStoreOpen = false
 
-if (cash > price) {
-    console.log(`you paid extra - here's ${difference} dollars in change`)
-}
-else if (cash === price) {
-    console.log("you paid the exact amout - have a nice day!")
-}
-else {
-    console.log(`not enough money - you still owe ${difference * -1} dollars`)
+if (cash >= price || isStoreOpen === true) {
+    console.log('print the receipt')
 }
