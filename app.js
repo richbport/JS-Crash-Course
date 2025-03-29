a9d4b2d466ba8201df3aa8a6dd9bc5dc8dbf60d8
Rich <richbport@yahoo.com>
2025-03-29 10:03:07 -0400

    conditional ternary operator

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-let subscribed = false
-let loggedIn = true
+let cash = 50;
+let price = 40;
+let isStoreOpen = true;
 
-let str = subscribed || loggedIn ? 'show the video' : 'hide the video'
-console.log(str);
+cash >= price && isStoreOpen
+  ? console.log("give receipt")
+  : console.log("do not give receipt");
+
+// 'give receipt' : 'do not give receipt'
