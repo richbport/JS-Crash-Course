50091c7f30368a60e17654493e1ad47a43300c59
Rich <richbport@yahoo.com>
2025-03-31 11:47:26 -0400

    loop exercise

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-let cash = 50;
-let price = 40;
-let isStoreOpen = true;
+let count = 1;
 
-let str = cash >= price && isStoreOpen ? "give receipt" : "do not give receipt";
-console.log(str);
+while (count <= 100) {
+    console.log(count)
+    count = count + 1;
+}
 
-// 'give receipt' : 'do not give receipt'
