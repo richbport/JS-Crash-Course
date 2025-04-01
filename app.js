4aa567a801db0f3734323b9a0c98376819dea733
Rich <richbport@yahoo.com>
2025-04-01 11:52:57 -0400

    new problem

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-for (let i = 1; i <= 20; ++i) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log(`${i} -> Frontend Simplified`);
-  } else if (i % 3 === 0) {
-    console.log(`${i} -> Frontend`);
-  } else if (i % 5 === 0) {
-    console.log(`${i} -> Simplified`);
-  } else {
-    console.log(`${i} -> ${i}`);
-  }
-}
+/** Print out every character from the string:
+ * "Frontend Simplified"
+ * 
+ * @example
+ * 'F'
+ * 'r'
+ * 'o'
+ * ...
+ * 'e'
+ * 'd'
+ */
