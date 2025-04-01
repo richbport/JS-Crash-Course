de654e6de63004a71e6eec873dda4c6d4487f5cc
Rich <richbport@yahoo.com>
2025-04-01 11:20:21 -0400

    line 2 correction

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 for (let i = 1; i <= 20; ++i) {
-  if (i % 3 === 0 && 1 % 5 === 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log(`${i} -> Frontend Simplified`);
   } 
   else if (i % 3 === 0) {
