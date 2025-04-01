3d15e3fe45893a75ae5c4ae90a80611e3512628c
Rich <richbport@yahoo.com>
2025-04-01 10:41:13 -0400

    loops exercise

--- app.js
+++ app.js
@@ -6,7 +6,13 @@
 // }
 
 for (let i = 1; i <= 20; ++i) {
-if (i % 3) {
-    console.log(i)
-}
+  if (i % 3 === 0) {
+    console.log(`${i} -> Frontend`);
+  }
+  if (i % 5 === 0) {
+    console.log(`${i} -> Simplified`)
+  }
+  else if (i % 3 === 0 && 1 % 5 === 0) {
+    console.log(`${i} -> Frontend Simplified`)
+  }
 }
