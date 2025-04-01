0c3b355f2e8cd31895e853587d2d6e745ff41789
Rich <richbport@yahoo.com>
2025-04-01 11:48:54 -0400

    retry with notes

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 for (let i = 1; i <= 20; ++i) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log(`${i} -> Frontend Simplified`);
-  } 
-  else if (i % 3 === 0) {
+  } else if (i % 3 === 0) {
     console.log(`${i} -> Frontend`);
-  } 
-  else if (i % 5 === 0) {
+  } else if (i % 5 === 0) {
     console.log(`${i} -> Simplified`);
-  } 
-  else {
+  } else {
     console.log(`${i} -> ${i}`);
   }
 }
