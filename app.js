038bd924a7a9b125d0c34b34b9c4929c49c81ad9
Rich <richbport@yahoo.com>
2025-04-02 13:23:33 -0400

    return keyword

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-// DRY - Don't repeat yourself
-
-// Function definition
-function welcomePersonToFES(firstName, lastName) {
-  console.log(`Welcome to FES, ${firstName} ${lastName}`);
+function fn() {
+    return 5
+  console.log("my function");
 }
 
-// Call the function
-welcomePersonToFES("Rich", "Port");
-welcomePersonToFES("David", "Bragg");
+console.log(fn());
