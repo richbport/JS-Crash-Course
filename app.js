4053483ad20e9bdabeafd481108b0d0483c3b057
Rich <richbport@yahoo.com>
2025-04-03 20:12:37 -0400

    array push

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 let arr = [20, 30, 40, 50, 100];
 
-console.log(arr[0]);
+// First element of an array:
+console.log(arr[0])
 
-console.log(arr[arr.length - 1]);
+// Last element of an array:
+console.log(arr[arr.length - 1])
+
+arr.push(200);
+
+console.log(arr);
