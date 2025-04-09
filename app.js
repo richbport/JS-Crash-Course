0f9243b3122a39e65ecf9245b34f2c80505f4518
Rich <richbport@yahoo.com>
2025-04-09 10:06:53 -0400

    for loop with push exercise

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ let dollars = [1, 5, 10, 3];
 let cents = [];
 
 for (let i = 0; i < dollars.length; ++i) {
-  console.log(dollars[i] * 100);
+  cents.push(dollars[i] * 100);
 }
+
+console.log(cents)
