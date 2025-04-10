ec727b9ca4fcc7a70c8ecaa53bb79a6eb9a810a0
Rich <richbport@yahoo.com>
2025-04-10 12:36:18 -0400

    object to an array

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-let user = {
-  username: "Rich",
-  email: "rich@frontendsimplified.com",
-  subscriptionStatus: "VIP",
-  discordId: "Rich Port#0001",
-  lessonsCompleted: [0, 1],
-}
+let user = [
+  {
+    username: "Rich",
+    email: "rich@frontendsimplified.com",
+    subscriptionStatus: "VIP",
+    discordId: "Rich Port#0001",
+    lessonsCompleted: [0, 1],
+  },
+  {
+    username: "Mitri",
+    email: "mitri@frontendsimplified.com",
+    subscriptionStatus: "VIP",
+    discordId: "Mitri#0001",
+    lessonsCompleted: [0, 1, 3, 4],
+  },
+];
 
 console.log(user.username[user.username.length - 1]);
 
-console.log(user.subscriptionStatus)
+console.log(user.subscriptionStatus);
 
-console.log(user.lessonsCompleted)
+console.log(user.lessonsCompleted.map((elem) => elem * 2));
