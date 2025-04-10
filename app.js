bb4a8b45bf7e3798a966c4d19b5dc8d3879947cf
Rich <richbport@yahoo.com>
2025-04-10 15:51:19 -0400

    for loop over an array

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let users = [
   {
     username: "Rich",
     email: "rich@frontendsimplified.com",
-    password: "test123"
+    password: "test123",
     subscriptionStatus: "VIP",
     discordId: "Rich Port#0001",
     lessonsCompleted: [0, 1],
@@ -10,10 +10,17 @@ let users = [
   {
     username: "Mitri",
     email: "mitri@frontendsimplified.com",
+    password: "mitri123",
     subscriptionStatus: "VIP",
     discordId: "Mitri#0001",
-    lessonsCompleted: [0, 1, 3, 4],
+    lessonsCompleted: [0, 1, 2, 3],
   },
 ];
 
-console.log(users[0].lessonsCompleted.map(elem => elem * 2))
+function login(email, password) {
+  for (let i = 0; i < users.length; ++i) {
+    console.log(users[i]);
+  }
+}
+
+login('rich@frontendsimplified.com', 'test123')
