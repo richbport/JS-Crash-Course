90b579ea1c83f93ba5555a29d1523b50386b3118
Rich <richbport@yahoo.com>
2025-04-10 16:32:32 -0400

    for loop password verification

--- app.js
+++ app.js
@@ -15,12 +15,25 @@ let users = [
     discordId: "Mitri#0001",
     lessonsCompleted: [0, 1, 2, 3],
   },
+  {
+    username: "Zen",
+    email: "zen@frontendsimplified.com",
+    password: "zen123",
+    subscriptionStatus: "VIP",
+    discordId: "Zen#0001",
+    lessonsCompleted: [0, 1, 2, 3],
+  },
 ];
 
 function login(email, password) {
   for (let i = 0; i < users.length; ++i) {
-    console.log(users[i]);
+    if (users[i].email === email) {
+      console.log(users[i]);
+      if (users[i].password === password) {
+        console.log('log the user in- the details are correct')
+      }
+    }
   }
 }
 
-login('rich@frontendsimplified.com', 'test123')
+login("rich@frontendsimplified.com", "test123");
