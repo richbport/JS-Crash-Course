cab9fa779fc642b06c317f7b12dd7e8e4b3a383c
Rich <richbport@yahoo.com>
2025-04-14 09:40:08 -0400

    DOM exercise

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // First way of accessing an element
-console.log(document.querySelector)
+console.log(document.querySelector('#title'));
 
 // Second way of accessing an element
+console.log(document.getElementById('title'))
