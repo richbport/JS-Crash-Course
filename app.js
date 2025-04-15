32f3c23598f957d6dfcd9a8891c4b563271a00a3
Rich <richbport@yahoo.com>
2025-04-15 17:17:14 -0400

    promise exercise

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+
+// 1. Then
+fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+  console.log(response.json())
+})
