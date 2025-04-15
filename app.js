f013a813b12c55434b6ff84f04edc47238e416bc
Rich <richbport@yahoo.com>
2025-04-15 17:20:50 -0400

    fetch exercise

--- app.js
+++ app.js
@@ -2,5 +2,7 @@
 
 // 1. Then
 fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-  console.log(response.json())
+  response.json().then(data => {
+    console.log(data)
+  })
 })
