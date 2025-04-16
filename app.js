63fc9e17501d5fe029c89f52cd192c046c6ac8ff
Rich <richbport@yahoo.com>
2025-04-16 18:14:15 -0400

    return exercise

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 // fetch("https://jsonplaceholder.typicode.com/users/1")
 const emailRef = document.querySelector(".email")
-console.log(emailRef)
 
 // 1. Then
 fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-  response.json().then(data => {
-    console.log(data)
-    emailRef.innerHTML = data.name;
-  })
+  console.log(response.json())
+  return response.json()
 })
+
