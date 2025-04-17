3d0be7dd3eeb0c76d0d46b982fa8d82f10723c89
Rich <richbport@yahoo.com>
2025-04-17 17:57:55 -0400

    async await continued

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const emailRef = document.querySelector(".email");
 // 2. Async/Await
 async function main() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+ const data = await response.json()
+  console.log(data)
+
 }
 
 main();
