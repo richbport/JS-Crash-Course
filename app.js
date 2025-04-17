f84f2886645504a8339da048d9d8e2d6681c04b4
Rich <richbport@yahoo.com>
2025-04-17 16:45:46 -0400

    async await

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 const emailRef = document.querySelector(".email");
 
 // 1. Then
-fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    emailRef.innerHTML = data.email
-  });
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email
+//   });
+
+// 2. Async/Await
+  async function main() {
+    await fetch("https://jsonplaceholder.typicode.com/users/1")
+  }
+
+  main()
